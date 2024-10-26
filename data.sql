126021551b9739e77b3b3b4cbdfba9ae8a1620ee
Dylan <dylanelias201931369@cunoc.edu.gt>
2024-10-26 13:58:26 -0600

    Arregla inserts actuales

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-INSERT INTO business (name, auto_assignement)
+INSERT INTO business (name, auto_assignment)
 VALUES ('company-test', FALSE);
 
 INSERT INTO role (name, description, multiuser)
@@ -7,8 +7,8 @@ VALUES ('ADMIN', 'Rol de administrador', TRUE);
 INSERT INTO role (name, description, multiuser)
 VALUES ('CLIENTE', 'Rol de cliente', TRUE);
 
-INSERT INTO user (email, password, name, lastname, business_id)
-VALUES ("example@example.com", "", "admin", "istrador", 1);
+INSERT INTO user (email, password, name, lastname, nit, cui, phone, active_mfa, mfa_secret, business_id)
+VALUES ('example@example.com', '', 'admin', 'istrador', '', '', '', '', FALSE, 1);
 
 INSERT INTO user_role (user_id, role_id)
 VALUES (1, 1);
