f10dfbfadae072719e546c60cd291ee18829e2a6
Dylan <dylanelias201931369@cunoc.edu.gt>
2024-10-26 22:43:43 -0600

    Cambia empresa default por zeday

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 INSERT INTO business (name, auto_assignment, logo_url)
-VALUES ('company-test', FALSE, 'https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_1280.png');
+VALUES ('ZeroDay', FALSE, 'https://www.creativefabrica.com/wp-content/uploads/2019/03/Monogram-ZD-Logo-Design-by-Greenlines-Studios.jpg');
 
 INSERT INTO role (name, description, multiuser)
 VALUES ('ADMIN', 'Rol de administrador', TRUE);
@@ -8,7 +8,7 @@ INSERT INTO role (name, description, multiuser)
 VALUES ('CLIENTE', 'Rol de cliente', TRUE);
 
 INSERT INTO user (email, password, name, lastname, nit, cui, phone, active_mfa, mfa_secret, business_id)
-VALUES ('example@example.com', '', 'admin', 'istrador', '', '', '', '', FALSE, 1);
+VALUES ('zeroday@example.com', '', 'admin', 'istrador', '', '', '', '', FALSE, 1);
 
 INSERT INTO user_role (user_id, role_id)
 VALUES (1, 1);
